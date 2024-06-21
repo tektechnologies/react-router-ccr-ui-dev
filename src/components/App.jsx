cd186596e86bae0a31456e621de01047bd572d8d
tektechnologies <info@tektechnologies.com>
2024-06-21 15:56:45 -0500

    add first react router lesson

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './NavBar';
+import Home from './Home';
+import Teams from './Teams';
+  import Players from './Players';
+
+
 export default function App () {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/players' element={<Players />}/>
+        <Route path='/teams' element={<Teams />}/>
+      </Routes>
+    </Router>
   )
 }
