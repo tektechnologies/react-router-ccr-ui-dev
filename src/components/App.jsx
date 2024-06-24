64c656eaa2df2bcdbcb036be643f6e0b32134324
tektechnologies <info@tektechnologies.com>
2024-06-24 15:57:33 -0500

    uncomment app comp and add map to render team logos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // FIRST LESSON //////////////////////////////////////////////////////////////////
-// import Navbar from './NavBar';
-// import Home from './Home';
-// import Teams from './Teams';
-// import Players from './Players';
+import Navbar from './NavBar';
+import Home from './Home';
+import Teams from './Teams';
+import Players from './Players';
 
 
-// export default function App () {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path='/' element={<Home />}/>
-//         <Route path='/players' element={<Players />}/>
-//         <Route path='/teams' element={<Teams />}/>
-//       </Routes>
-//     </Router>
-//   )
-// }
+export default function App () {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/players' element={<Players />}/>
+        <Route path='/teams' element={<Teams />}/>
+      </Routes>
+    </Router>
+  )
+}
 // FIRST LESSON //////////////////////////////////////////////////////////////////
 
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,6 +23,13 @@ export default function Home() {
             <h3 className='header text-center'>
                 Select a team
             </h3>
+            <div className='home-grid'>
+                {teamNames.map((id) => (
+                        <Link key={id} to={`/${id}`}>
+                            <TeamLogo id={id} width='125px' />
+                        </Link>
+                    ))}
+            </div>
         </div>
     )
 }
